d3a06cf2ced7e9c373360442243b4f154bd26693
MooSeon Park <66548209+mooseonpark@users.noreply.github.com>
2022-09-08 14:39:46 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 128 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync("/dev/stdin").toString().trim().split(" ").map(Number);
+
+const chess = [1, 1, 2, 2, 2, 8];
+
+let answer = input.map((i, index) => {
+  return chess[index] - i;
+})
+
+console.log(...answer) 
