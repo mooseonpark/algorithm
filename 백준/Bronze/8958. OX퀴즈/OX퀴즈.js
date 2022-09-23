684ce93b24a13783caaf39bc9b24a78bff4a6720
MooSeon Park <66548209+mooseonpark@users.noreply.github.com>
2022-09-23 16:06:00 +0900

    [Bronze II] Title: OX퀴즈, Time: 140 ms, Memory: 9576 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.js
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.js
@@ -1,18 +1,14 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
-
-const count = +input[0];
-
-for(let i=1; i<=count; i++){
-    let count = 1;
-    let sum = 0;
-    
-    for(let j=0; j<input[i].length; j++){
-        if(input[i][j]==="O"){
-            sum+=count;
-            count++;
-        }else{
-            count = 1;
-        }
-    }
-    console.log(sum);
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+for (let i = 1; i <= +input[0]; i++) {
+	let count = 0;
+	let sum = 0;
+	for (let j = 0; j < input[i].length; j++) {
+		if (input[i][j] === 'O') {
+			count++;
+			sum = sum + count;
+		} else {
+			count = 0;
+		}
+	}
+	console.log(sum);
 }
