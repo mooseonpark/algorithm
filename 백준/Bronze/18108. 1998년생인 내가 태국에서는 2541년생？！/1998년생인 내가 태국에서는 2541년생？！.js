8b2b6b1b124ca6c26a87f2ef8a3366b927f9a5fe
MooSeon Park <66548209+mooseonpark@users.noreply.github.com>
2022-10-08 16:20:14 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 132 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.js
@@ -1,3 +1,2 @@
-const input = require('fs').readFileSync("/dev/stdin").toString().trim();
-
-console.log(+input - 543)
+let input = +require('fs').readFileSync('/dev/stdin').toString();
+console.log(input - 543);
