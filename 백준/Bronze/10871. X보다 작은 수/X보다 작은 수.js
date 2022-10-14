c21cd3d7864ddfab542f906babeda4ea923d1c8e
MooSeon Park <66548209+mooseonpark@users.noreply.github.com>
2022-10-14 16:37:08 +0900

    [Bronze V] Title: X보다 작은 수, Time: 128 ms, Memory: 12256 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -1,14 +1,10 @@
 let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-
-const count = input[0].split(' ')[0];
-const num = +input[0].split(' ')[1]; 
-const num2 = input[1].split(' '); 
+let [count, num] = input[0].split(' ');
+let number = input[1].split(' ');
 let answer = '';
-
-for (let i = 0; i < count; i++) {
-	if (+num2[i] < +num) {
-		answer += +num2[i] + ' ';
+for (let i = 0; i < +count; i++) {
+	if (number[i] < +num) {
+		answer += number[i] + ' ';
 	}
 }
-
 console.log(answer);
