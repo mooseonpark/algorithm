2a745c2fd8c9b51acb3f82aa38f506304b8f8a3f
MooSeon Park <66548209+mooseonpark@users.noreply.github.com>
2022-10-20 12:32:19 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120817. 배열의 평균값/배열의 평균값.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    var answer = 0;
+//     for(let i=0; i<numbers.length;i++){
+//     answer += numbers[i];
+        
+//     }
+//     answer = answer/numbers.length;
+    let arr = numbers.reduce((prev,curr) => prev+curr,answer)
+    answer = arr/numbers.length;
+    return answer;
+}
