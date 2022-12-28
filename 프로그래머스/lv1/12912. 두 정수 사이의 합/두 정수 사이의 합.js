61ea4d37db7a62658aac34fdca4d19224e4ee00a
MooSeon Park <66548209+mooseonpark@users.noreply.github.com>
2022-12-28 13:17:15 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -0,0 +1,17 @@
+function solution(a, b) {
+    var answer = 0;
+    if(a===b){
+        answer=a;
+    }else if(a<b){
+        for(let i=a; i<=b;i++){
+            answer+=i
+        }        
+    }else if(a>b){
+        for(let i=a; i>=b;i--){
+            answer+=i
+        }
+    }
+
+
+    return answer;
+}
